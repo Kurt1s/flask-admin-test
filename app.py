c4f53ba72114c0c0df92c64a4362b1049b24244e
kurt1s <knbsk8@gmail.com>
2019-03-19 19:37:47 -0400

    Committed all work thus far

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin.contrib.sqla import ModelView
+# from flask_admin.contrib.sqla import ModelView
+
+app = Flask(__name__)
+
+# set optional bootswatch theme
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/kurtis/SoftEng/flask-admin-test/admin.db'
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+# DB initialization
+db = SQLAlchemy(app)
+
+#ORM Models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+
+
+@app.route("/")
+def index():
+    return render_template("register.html")
+
+
+# Flask and Flask-SQLAlchemy initialization here
+admin = Admin(app, name='Flask-Admin', template_mode='bootstrap3')
+
+admin.add_view(ModelView(User, db.session))
+
+if __name__ == '__main__':
+    app.run(debug=True)
